5a92830f65e5b29bdde3021c255ec90729947fbb
mahmud <mahmud120398@gmail.com>
2022-12-25 19:20:15 +0700

    refactor: update watchlist page test

--- test/presentation/pages/watchlist_page_test.dart
+++ test/presentation/pages/watchlist_page_test.dart
@@ -1,68 +1,114 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mocktail/mocktail.dart';
-// import 'package:provider/provider.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:ditonton/presentation/pages/watchlist_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:movie_feature/movie_feature.dart';
+import 'package:tv_feature/tv_feature.dart';
 
-// import 'watchlist_page_test.mocks.dart';
+class MockWatchlistMovieCubit extends MockCubit<WatchlistMovieState>
+    implements WatchlistMovieCubit {}
 
-// void main() {
-//   late MockWatchlistMovieNotifier watchlistMovieNotifier;
-//   late MockWatchlistTvNotifier watchlistTvNotifier;
+class MockWatchlistTvCubit extends MockCubit<TvWatchlistState>
+    implements TvWatchlistCubit {}
 
-//   setUp(() {
-//     watchlistMovieNotifier = MockWatchlistMovieNotifier();
-//     watchlistTvNotifier = MockWatchlistTvNotifier();
-//   });
+void main() {
+  late MockWatchlistMovieCubit mockWatchlistMovieCubit;
+  late MockWatchlistTvCubit mockWatchlistTvCubit;
 
-//   Widget _makeTestableWidget(Widget body) {
-//     return MultiProvider(
-//       providers: [
-//         ChangeNotifierProvider<WatchlistMovieNotifier>.value(
-//           value: watchlistMovieNotifier,
-//         ),
-//         ChangeNotifierProvider<WatchlistTvNotifier>.value(
-//           value: watchlistTvNotifier,
-//         ),
-//       ],
-//       child: MaterialApp(
-//         home: Scaffold(
-//           body: body,
-//         ),
-//       ),
-//     );
-//   }
+  setUp(() {
+    mockWatchlistMovieCubit = MockWatchlistMovieCubit();
+    mockWatchlistTvCubit = MockWatchlistTvCubit();
+  });
 
-//   testWidgets(
-//     'Should have tab movie and tv watchlist',
-//     (tester) async {
-//       when(watchlistMovieNotifier.watchlistState).thenReturn(
-//         RequestState.Loaded,
-//       );
-//       when(watchlistMovieNotifier.watchlistMovies).thenReturn(
-//         testMovieList,
-//       );
+  final testTv = Tv(
+    backdropPath: 'backdropPath',
+    firstAirDate: "2021-05-23",
+    genreIds: const [1, 2, 3],
+    id: 1,
+    name: 'name',
+    originCountry: const ['en'],
+    originalLanguage: 'originalLanguage',
+    originalName: 'originalName',
+    overview: 'overview',
+    popularity: 1.0,
+    posterPath: 'posterPath',
+    voteAverage: 1.0,
+    voteCount: 1,
+  );
 
-//       when(watchlistTvNotifier.watchlistState).thenReturn(
-//         RequestState.Loaded,
-//       );
+  final testMovie = Movie(
+    adult: false,
+    backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
+    genreIds: const [14, 28],
+    id: 557,
+    originalTitle: 'Spider-Man',
+    overview:
+        'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+    popularity: 60.441,
+    posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+    releaseDate: '2002-05-01',
+    title: 'Spider-Man',
+    video: false,
+    voteAverage: 7.2,
+    voteCount: 13507,
+  );
 
-//       when(watchlistTvNotifier.watchlistTv).thenReturn(
-//         testTvList,
-//       );
+  final testMovieList = [testMovie];
+  final testTvList = [testTv];
 
-//       await tester.pumpWidget(_makeTestableWidget(WhatchlistPage()));
+  Widget _makeTestableWidget(Widget body) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<WatchlistMovieCubit>.value(value: mockWatchlistMovieCubit),
+        BlocProvider<TvWatchlistCubit>.value(value: mockWatchlistTvCubit),
+      ],
+      child: MaterialApp(
+        home: body,
+      ),
+    );
+  }
 
-//       var tvWatchlist = find.byType(TvWatchlistPage);
-//       var movieWatchlist = find.byType(WatchlistMoviesPage);
+  void provideStubProvider({
+    required WatchlistMovieState watchlistMovieState,
+    required TvWatchlistState tvWatchlistState,
+  }) {
+    when(() => mockWatchlistMovieCubit.state).thenReturn(watchlistMovieState);
+    when(() => mockWatchlistTvCubit.state).thenReturn(tvWatchlistState);
 
-//       expect(tvWatchlist, findsNothing);
-//       expect(movieWatchlist, findsOneWidget);
+    when(() => mockWatchlistMovieCubit.loadMovieWatchlist()).thenAnswer(
+      (_) async => {},
+    );
 
-//       var tabView = find.byType(TabBarView);
-//       var tabController = DefaultTabController.of(tester.element(tabView));
+    when(() => mockWatchlistTvCubit.loadTvWatchlist()).thenAnswer(
+      (_) async => {},
+    );
+  }
 
-//       expect(tabController?.length, equals(2));
-//       expect(tabController?.index, 0);
-//     },
-//   );
-// }
+  testWidgets(
+    'Should have tab movie and tv watchlist',
+    (tester) async {
+      /// stub widget
+      provideStubProvider(
+        watchlistMovieState: WatchlistMovieDataSuccess(movies: testMovieList),
+        tvWatchlistState: TvWatchlistSuccess(tv: testTvList),
+      );
+
+      /// pump widget
+      await tester.pumpWidget(_makeTestableWidget(WatchlistPage()));
+
+      var tvWatchlist = find.byType(TvWatchlistPage);
+      var movieWatchlist = find.byType(WatchlistMoviesPage);
+
+      expect(tvWatchlist, findsNothing);
+      expect(movieWatchlist, findsOneWidget);
+
+      var tabView = find.byType(TabBarView);
+      var tabController = DefaultTabController.of(tester.element(tabView));
+
+      expect(tabController?.length, equals(2));
+      expect(tabController?.index, 0);
+    },
+  );
+}
