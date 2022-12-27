24b580aea8e23190b043571864126384fa34546b
mahmud <mahmud120398@gmail.com>
2022-12-27 11:54:21 +0700

    refactor: update background

--- lib/presentation/pages/about_page.dart
+++ lib/presentation/pages/about_page.dart
@@ -23,12 +23,13 @@ class AboutPage extends StatelessWidget {
                 ),
               ),
               Expanded(
+                flex: 2,
                 child: Container(
-                  padding: const EdgeInsets.all(32.0),
-                  color: kMikadoYellow,
+                  padding: const EdgeInsets.all(16.0),
+                  color: Colors.white,
                   child: Text(
                     'Ditonton merupakan sebuah aplikasi katalog film yang dikembangkan oleh Dicoding Indonesia sebagai contoh proyek aplikasi untuk kelas Menjadi Flutter Developer Expert.',
-                    style: TextStyle(color: Colors.black87, fontSize: 16),
+                    style: TextStyle(color: Colors.black87, fontSize: 14),
                     textAlign: TextAlign.justify,
                   ),
                 ),
