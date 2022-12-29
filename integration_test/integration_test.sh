d3c294ab935c117c1f43a041d78bd1763e63428e
mahmud <mahmud120398@gmail.com>
2022-12-29 08:50:26 +0700

    feat: add integration test

---
+++ integration_test/integration_test.sh
@@ -0,0 +1,2 @@
+flutter drive --driver=test_driver/app_driver.dart --target=integration_test/movie_feature_test.dart
+flutter drive --driver=test_driver/app_driver.dart --target=integration_test/tv_feature_test.dart
